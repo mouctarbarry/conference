bc50307cdf2fd529050e8dc026150f1a147a99ff
mouctarbarry <mouctar.barry@sogeti.com>
2023-11-01 18:37:46 +0100

    init project

---
+++ src/main/java/com/mouctar/model/Speaker.java
@@ -0,0 +1,12 @@
+package com.mouctar.model;
+
+public class Speaker {
+
+    private String lastname;
+    private String firstname;
+
+    public Speaker(String lastname, String firstname) {
+        this.lastname = lastname;
+        this.firstname = firstname;
+    }
+}
